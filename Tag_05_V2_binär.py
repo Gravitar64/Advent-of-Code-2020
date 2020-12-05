1810e0c1148d6fddd11cc83a46283f15dc9c6322
Gravitar64 <gravitar@web.de>
2020-12-05 23:55:00 +0100

    2-Zeilen Version von Brother Lui

--- Tag_05_V2_binär.py
+++ Tag_05_V2_binär.py
@@ -1,17 +1,9 @@
+#Idee von Brother Lui
 import time
 
-def puzzle_einlesen(datei):
-  with open(datei) as f:
-    return [int(''.join(['0' if c in 'FL' else '1' for c in zeile.strip()]),2) for zeile in f]
-  
-def löse(puzzle):
-  puzzle.sort()
-  for sid1,sid2 in zip(puzzle,puzzle[1:]):
-    if sid2-sid1 != 1:
-      sid = sid2-1
-      break
-  return puzzle[-1], sid 
-  
-puzzle = puzzle_einlesen('Tag_05.txt')
 start = time.perf_counter()
-print(löse(puzzle), time.perf_counter()-start)
+
+S = set(int(s.translate({70:48,66:49,76:48,82:49}), 2) for s in open("Tag_05.txt"))
+print(max(S), set(range(min(S), max(S))) - S)
+
+print(time.perf_counter()-start)
