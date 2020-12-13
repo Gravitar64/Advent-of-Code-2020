22a64c0970478174539b6603c40e30625077b165
Gravitar64 <gravitar@web.de>
2020-12-13 21:03:34 +0100

    international version

--- Tag_13.py
+++ Tag_13.py
@@ -1,35 +1,34 @@
 import time
 
-def puzzle_einlesen(datei):
-  with open(datei) as f:
+def read_puzzle(file):
+  with open(file) as f:
     ts = f.readline()
     ids = [x for x in f.readline().split(',')]
     return int(ts), ids
 
 
-def löse(ts,ids):
+def solve1(ts,ids):
   ids = [int(id) for id in ids if id != 'x']
   diff,id = sorted([(id - ts % id, id) for id in ids])[0]
   return diff * id
 
 
-def löse2(ids):
-  ids = [(start,int(id)) for start, id in enumerate(ids) if id != 'x']
-  position = 0
-  kgV = 1
+def solve2(ids):
+  ids = [(start, int(id)) for start,id in enumerate(ids) if id != 'x']
+  ts = 0
+  lcm = 1
   for start, step in ids:
-    #print(f'Bus {step} startet bei TS {start}')
-    while (position + start) % step != 0:
-      position += kgV
-    kgV *= step
-    #print(f'Position: {position}, kgV: {kgV}')
-  return position  
+    while(ts + start) % step != 0:
+      ts += lcm
+    lcm *= step
+  return ts    
+  
 
 
-ts, ids = puzzle_einlesen('Tag_13.txt')
-
-start = time.perf_counter()
-print(löse(ts,ids),  time.perf_counter()-start)
+ts, ids = read_puzzle('Tag_13.txt')
 
 start = time.perf_counter()
-print(löse2(ids),  time.perf_counter()-start)
+print(solve1(ts,ids),  time.perf_counter()-start)
+
+start = time.perf_counter()
+print(solve2(ids),  time.perf_counter()-start)
