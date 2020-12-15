56fa435cd9eb236fe71c08c0abe2ad5637aa1994
Gravitar64 <gravitar@web.de>
2020-12-15 14:25:42 +0100

    YouTube2

--- Tag_15.py
+++ Tag_15.py
@@ -1,34 +1,34 @@
 import time
-import copy
 from collections import defaultdict
+import copy
 
 def read_puzzle(file):
   p = defaultdict(list)
   with open(file) as f:
     for i,num in enumerate(f.readline().split(',')):
       p[int(num)].append(i+1)
-  return p  
+  return p
   
 
-def solve(p,lastTurn):
-  last = list(p.keys())[-1]
-  turn = len(p)
-  while turn < lastTurn:
-    turn += 1
-    if len(p[last]) > 1:
-      akt = p[last][-1] - p[last][-2]
+def solve(p,last_round):
+  last_num = list(p.keys())[-1]
+  runde = len(p)
+  while runde < last_round:
+    runde +=1
+    if len(p[last_num]) > 1:
+      akt = p[last_num][-1] - p[last_num][-2]
     else:
       akt = 0
-    p[akt].append(turn)
-    last = akt
+    p[akt].append(runde)
+    last_num = akt
   return akt      
 
 
 puzzle = read_puzzle('Tag_15.txt')
 
 start = time.perf_counter()
-print(solve(copy.deepcopy(puzzle),2020),  time.perf_counter()-start)
+print(solve(copy.deepcopy(puzzle),2020),time.perf_counter()-start)
 
 start = time.perf_counter()
-print(solve(puzzle,30_000_000),  time.perf_counter()-start)
+print(solve(puzzle,30_000_000),time.perf_counter()-start)
 
