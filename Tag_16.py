a0e225a5c2cf8d6f7aa1b3ae7fe12cded6d83a7e
Gravitar64 <gravitar@web.de>
2020-12-16 22:00:42 +0100

    YouTube-Version

--- Tag_16.py
+++ Tag_16.py
@@ -1,75 +1,56 @@
-from os import truncate
-import time
+import time, re, math
 from collections import defaultdict
 
 def read_puzzle(file):
-  rules, myTicket, tickets = defaultdict(set), [], []
+  valids = set()
+  rule_dict = {}
+
   with open(file) as f:
-    part = 0
-    for z in [x.strip() for x in f]:
-      if not z:
-        part += 1
-        continue
-      if part == 0:
-        ruleName,p2 = z.split(': ')
-        p1,p2 = p2.split(' or ')
-        p3,p4 = p1.split('-')
-        p5,p6 = p2.split('-')
-        rules[ruleName].update(range(int(p3),int(p4)+1))
-        rules[ruleName].update(range(int(p5),int(p6)+1))
-      elif part == 1:
-        myTicket = [int(x) for x in z.split(',')]
-      else:
-        tickets.append([int(x) for x in z.split(',')])
-  return rules, myTicket, tickets      
+    rules, mine, nearby = f.read().split('\n\n')
+  
+  for zeile in rules.splitlines():
+    name, regeln = zeile.split(': ')
+    l = [int(x) for x in re.findall('\d+', regeln)]
+    v = set(range(l[0],l[1]+1)) | set(range(l[2],l[3]+1))
+    valids.update(v)
+    rule_dict[name] = v
 
 
-    
 
-def solve(rules,tickets):
-  all_rules = {x for s in rules.values() for x in s}
-  return sum([field for t in tickets for field in t if field not in all_rules])
+  tickets = [[int(x) for x in re.findall('\d+', mine)]]
+  tickets.extend([[int(x) for x in re.findall('\d+', t)] for t in nearby.split('\n')[1:]])
+  return valids, tickets, rule_dict  
 
 
-def solve2(rules,myTicket,tickets):
-  positions = defaultdict(list)
-  all_rules = {x for s in rules.values() for x in s}
-  delTickets = [i for i,t in enumerate(tickets) for field in t if field not in all_rules]
-  for i in reversed(delTickets):
-    del tickets[i]
-  tickets.append(myTicket)
-  for name,rule in rules.items():
+def solve(valids, tickets, rule_dict):
+  par1 = sum([feld for t in tickets for feld in t if feld not in valids])
+
+  for t in reversed(tickets):
+    if all([feld in valids for feld in t]): continue
+    tickets.remove(t)
+  
+  poss = defaultdict(list)
+  for name, rule in rule_dict.items():
     for pos in range(len(tickets[0])):
-      if all([t[pos] in rule for t in tickets]):
-        positions[name].append(pos)
+      if not {t[pos] for t in tickets}.issubset(rule): continue
+      poss[name].append(pos)
+
   while True:
     change = False
-    for name,poss in positions.items():
-      if len(poss) == 1:
-        for n1,p2 in positions.items():
-          if len(p2) == 1: continue
-          if poss[0] in p2:
-            positions[n1].remove(poss[0])
-            change = True
+    ones = [pos[0] for pos in poss.values() if len(pos) == 1]
+    for one in ones:
+      for name, pos in poss.items():
+        if len(pos) == 1: continue
+        if one in pos:
+          poss[name].remove(one)
+          change = True
     if not change:
       break
-  product = 1
-  for name,idx in positions.items():
-    if 'departure' not in name: continue
-    product *= myTicket[idx[0]]
-  return product  
+  myTicket = tickets[0]
+  part2 = math.prod([myTicket[i[0]] for n,i in poss.items() if 'departure' in n])
+  return par1, part2 
 
-
-
-
-
-           
-  
-
-rules, myTicket, tickets = read_puzzle('Tag_16.txt')
+puzzle = read_puzzle('Tag_16.txt')
 
 start = time.perf_counter()
-print(solve(rules, tickets),time.perf_counter()-start)
-
-start = time.perf_counter()
-print(solve2(rules,myTicket,tickets),time.perf_counter()-start)
+print(solve(*puzzle), time.perf_counter()-start)
