d30c9d96711abfd64f43bf754b72d2425ef36c04
Gravitar64 <gravitar@web.de>
2020-12-17 23:22:51 +0100

    The faster one

--- Tag_17.py
+++ Tag_17.py
@@ -3,28 +3,19 @@ from collections import Counter
 import itertools as iter
 
 
-class Vec(tuple):
-  def __new__(cls, *args):
-    return tuple.__new__(cls, args)
-
-  def __add__(self, other):
-    return Vec(*tuple(a+b for a, b in zip(self, other)))
-
-
 def puzzle_einlesen(datei):
   with open(datei) as f:
     return [x.strip() for x in f]
 
 
 def nachbarn(pos):
-  for delta in iter.product(range(-1, 2), repeat=len(pos)):
-    if all([n == 0 for n in delta]):
-      continue
-    yield pos + Vec(*delta)
+   for delta in iter.product(range(-1, 2), repeat=len(pos)):
+    if delta ==  (0,) * len(pos): continue
+    yield tuple([a+b for a,b in zip(pos, delta)])
 
 
 def löse(puzzle, dim):
-  karte = {Vec(*(x, y)+(0,)*(dim-2)) for y, z in enumerate(puzzle)
+  karte = {tuple([x, y]+[0]*(dim-2)) for y, z in enumerate(puzzle)
            for x, c in enumerate(z) if c == '#'}
   for _ in range(6):
     nachb = Counter([p for pos in karte for p in nachbarn(pos)])
@@ -35,8 +26,6 @@ def löse(puzzle, dim):
 
 puzzle = puzzle_einlesen('Tag_17.txt')
 
-start = time.perf_counter()
-print(löse(puzzle, 3), time.perf_counter()-start)
-
-start = time.perf_counter()
-print(löse(puzzle, 4), time.perf_counter()-start)
+for dim in range(3,4):
+  start = time.perf_counter()
+  print(löse(puzzle, dim), time.perf_counter()-start)
