e1f0ccebf0700d08cafb87f547fcaa34ee68abb9
Gravitar64 <gravitar@web.de>
2020-12-17 23:24:41 +0100

    International Version

--- Tag_17.py
+++ Tag_17.py
@@ -3,29 +3,29 @@ from collections import Counter
 import itertools as iter
 
 
-def puzzle_einlesen(datei):
-  with open(datei) as f:
+def read_puzzle(file):
+  with open(file) as f:
     return [x.strip() for x in f]
 
 
-def nachbarn(pos):
+def neighbors(pos):
    for delta in iter.product(range(-1, 2), repeat=len(pos)):
     if delta ==  (0,) * len(pos): continue
     yield tuple([a+b for a,b in zip(pos, delta)])
 
 
-def löse(puzzle, dim):
-  karte = {tuple([x, y]+[0]*(dim-2)) for y, z in enumerate(puzzle)
+def solve(puzzle, dim):
+  active = {tuple([x, y]+[0]*(dim-2)) for y, z in enumerate(puzzle)
            for x, c in enumerate(z) if c == '#'}
   for _ in range(6):
-    nachb = Counter([p for pos in karte for p in nachbarn(pos)])
-    karte = {pos for pos, anz in nachb.items() if anz ==
-             3 or (anz == 2 and pos in karte)}
-  return len(karte)
+    neighb = Counter([p for pos in active for p in neighbors(pos)])
+    active = {pos for pos, anz in neighb.items() if anz ==
+             3 or (anz == 2 and pos in active)}
+  return len(active)
 
 
-puzzle = puzzle_einlesen('Tag_17.txt')
+puzzle = read_puzzle('Tag_17.txt')
 
-for dim in range(3,4):
+for dim in range(3,5):
   start = time.perf_counter()
-  print(löse(puzzle, dim), time.perf_counter()-start)
+  print(solve(puzzle, dim), time.perf_counter()-start)
