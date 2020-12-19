3d23a43adf2f145e04a6a3e29e5750ac3d4cdcb7
Gravitar64 <gravitar@web.de>
2020-12-19 17:56:38 +0100

    leicht optimiert

--- Tag_19.py
+++ Tag_19.py
@@ -1,27 +1,32 @@
-import time, re
+import time
+import re
+
 
 def read_puzzle(file):
   with open(file) as f:
     return f.read().split('\n\n')
 
+
 def dfs(tree, node, depth):
   if depth > 20: return ''
-  suchstring = '('
-  if tree[node][0] == '"': return tree[node][1]
-  for i,s in enumerate(tree[node].split('|')):
-    if i == 1: suchstring += '|'
-    for t in s.split():
-      suchstring += dfs(tree, t, depth+1)
-  return suchstring + ')'    
+  seachstr = '('
+  for next in tree[node].split():
+    if next == '|':
+      searchstr += next
+    elif next.isdigit():
+      searchstr += dfs(tree, next, depth+1)
+    else:
+      return next[1]  
+  return searchstr+')'
 
 
-    
-def solve(rules,messages,part1=True):
+def solve(rules, messages, part1=True):
   if not part1:
     rules += '\n8: 42 | 42 8\n11: 42 31 | 42 11 31'
   rules = dict([zeile.split(': ') for zeile in rules.split('\n')])
-  r = re.compile(dfs(rules,'0',0))
+  r = re.compile(dfs(rules, '0', 0))
   return sum([1 for m in messages.split() if r.fullmatch(m)])
+
 
 puzzle = read_puzzle('Tag_19.txt')
 
