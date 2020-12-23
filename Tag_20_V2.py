396d9f2af1545ef0d7e4add3b97b8e03465ed3ac
Gravitar64 <gravitar@web.de>
2020-12-23 13:05:17 +0100

    leichte Optimierungen (GRID_SIZE, TILE_SIZE :=)

--- Tag_20_V2.py
+++ Tag_20_V2.py
@@ -22,13 +22,13 @@ def get_border(tile, r):
   if r == 'r': return ''.join([zeile[-1] for zeile in tile])
 
 
-def create_map(solution, tilesV, size):
+def create_map(solution, tilesV, GRID_SIZE, TILE_SIZE):
   karte = []
-  for start in range(0, len(solution), size):
-    for y in range(1, 9):
+  for start in range(0, len(solution), GRID_SIZE):
+    for y in range(1, TILE_SIZE-1):
       zeile = ''
-      for offsX in range(size):
-        zeile += tilesV[solution[start+offsX]][y][1:9]
+      for offsX in range(GRID_SIZE):
+        zeile += tilesV[solution[start+offsX]][y][1:TILE_SIZE-1]
       karte.append(zeile)
   return karte
 
@@ -37,34 +37,31 @@ def solve(puzzle):
   tiles = {tile[0][5: -1]: tile[1:] for tile in puzzle}
   tilesV = {idv: tile_v for id, tile in tiles.items()
             for idv, tile_v in get_vars(id, tile)}
-  SIZE = int(len(tiles)**0.5)
+  GRID_SIZE = int(len(tiles)**0.5)
+  TILE_SIZE = len(list(tiles.values())[0])
 
   def match_tiles(pos, idV, order):
-    matchH = pos % SIZE == 0 or get_border(
+    matchH = pos % GRID_SIZE == 0 or get_border(
         tilesV[idV], 'l') == get_border(tilesV[order[-1]], 'r')
-    matchV = pos < SIZE or get_border(
-        tilesV[idV], 'u') == get_border(tilesV[order[-SIZE]], 'd')
+    matchV = pos < GRID_SIZE or get_border(
+        tilesV[idV], 'u') == get_border(tilesV[order[-GRID_SIZE]], 'd')
     return matchH and matchV
 
   def dfs(placed_tiles, seen):
-    if len(placed_tiles) == len(tiles):
-      return placed_tiles
-    pos = len(placed_tiles)
+    if (pos := len(placed_tiles)) == len(tiles): return placed_tiles
     for idV in tilesV:
-      if idV[0] in seen:
-        continue
+      if idV[0] in seen: continue
       if match_tiles(pos, idV, placed_tiles):
-        solution = dfs(placed_tiles + [idV], seen | {idV[0]})
-        if solution:
+        if (solution := dfs(placed_tiles + [idV], seen | {idV[0]})): 
           return solution
 
   solution = dfs([], set())
   part1 = math.prod(int(s[0]) for s in [solution[0],
-                                        solution[-1], solution[SIZE-1], solution[-SIZE]])
+                                        solution[-1], solution[GRID_SIZE-1], solution[-GRID_SIZE]])
 
-  spacing = f'[.#\n]{{{SIZE*8-19}}}'
+  spacing = f'[.#\n]{{{GRID_SIZE*(TILE_SIZE-2)-19}}}'
   monster = f'#.{spacing+"#....#"*3}##{spacing}.#{"..#"*5}'
-  karte = create_map(solution, tilesV, SIZE)
+  karte = create_map(solution, tilesV, GRID_SIZE, TILE_SIZE)
 
   for _, karteV in get_vars('a', karte):
     karteStr = '\n'.join(karteV)
