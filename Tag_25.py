913cb690c9814b102dd9b585d70820137e28a8fa
Gravitar64 <gravitar@web.de>
2020-12-26 14:15:19 +0100

    default-values in find_loop

--- Tag_25.py
+++ Tag_25.py
@@ -6,14 +6,13 @@ def read_puzzle(file):
     return [int(line.strip()) for line in f]
 
 
-def find_loop(pub, value, loop):
+def find_loop(pub, value=1, loop=0):
   while value != pub:
     value, loop = (value * 7) % DIVIDER, loop + 1
   return loop
 
-
 def solve(puzzle):
-  return pow(puzzle[1], find_loop(puzzle[0], 1, 0), DIVIDER)
+  return pow(puzzle[1], find_loop(puzzle[0]), DIVIDER)
 
 
 puzzle = read_puzzle('Tag_25.txt')
