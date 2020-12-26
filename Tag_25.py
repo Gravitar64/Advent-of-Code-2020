71c9180505a3e220b10c402bf06f64d672444f1f
Gravitar64 <gravitar@web.de>
2020-12-26 14:04:03 +0100

    Perf-Update (POW(basis, exponent, modulus)

--- Tag_25.py
+++ Tag_25.py
@@ -6,25 +6,18 @@ def read_puzzle(file):
     return [int(line.strip()) for line in f]
 
 
-def find_loop(pub_key, value, loop):
-  while value != pub_key:
-    loop += 1
-    value = (value * 7) % 20201227
+def find_loop(pub, value, loop):
+  while value != pub:
+    value, loop = (value * 7) % DIVIDER, loop + 1
   return loop
 
 
-def gen_secret(pub_key, loop, value):
-  for _ in range(loop):
-    value = (value * pub_key) % 20201227
-  return value
-
-
 def solve(puzzle):
-  l = find_loop(puzzle[0],1,0)
-  return gen_secret(puzzle[1],l,1)
+  return pow(puzzle[1], find_loop(puzzle[0], 1, 0), DIVIDER)
 
 
 puzzle = read_puzzle('Tag_25.txt')
+DIVIDER = 20201227
 
 start = time.perf_counter()
 print(solve(puzzle), time.perf_counter()-start)
