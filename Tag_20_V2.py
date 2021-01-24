66aed0d6ba0971782c9271c7a731d88ce1179f8f
Gravitar64 <gravitar@web.de>
2021-01-24 18:30:20 +0100

    Optimiert

--- Tag_20_V2.py
+++ Tag_20_V2.py
@@ -2,72 +2,58 @@ import time
 import math
 import regex as re
 
-
 def read_puzzle(file):
   with open(file) as f:
-    return [[x for x in tile.split('\n')]for tile in f.read().split('\n\n')]
+    return [[x for x in tile.split('\n')] for tile in f.read().split('\n\n')]
 
-
-def get_vars(id, tile):
+def rot_flip(id, tile):
   for i in range(4):
-    yield (id, i), tile
-    yield (id, i+4), [zeile for zeile in reversed(tile)]  # flipH
-    tile = list(map("".join, zip(*tile[::-1])))  # rotate 90 cw
-
-
-def get_border(tile, r):
-  if r == 'u': return tile[0]
-  if r == 'd': return tile[-1]
-  if r == 'l': return ''.join([zeile[0] for zeile in tile])
-  if r == 'r': return ''.join([zeile[-1] for zeile in tile])
-
+    yield (id,i), tile
+    yield (id,i+4), tile[::-1] #flipV
+    tile = list(map(''.join, zip(*tile[::-1]))) # rotate 90 degrees cw
 
 def create_map(solution, tilesV, GRID_SIZE, TILE_SIZE):
   karte = []
   for start in range(0, len(solution), GRID_SIZE):
     for y in range(1, TILE_SIZE-1):
       zeile = ''
-      for offsX in range(GRID_SIZE):
-        zeile += tilesV[solution[start+offsX]][y][1:TILE_SIZE-1]
+      for x in range(GRID_SIZE):
+        zeile += tilesV[solution[start+x]][y][1:TILE_SIZE-1]
       karte.append(zeile)
-  return karte
-
+  return karte      
 
 def solve(puzzle):
-  tiles = {tile[0][5: -1]: tile[1:] for tile in puzzle}
-  tilesV = {idv: tile_v for id, tile in tiles.items()
-            for idv, tile_v in get_vars(id, tile)}
-  GRID_SIZE = int(len(tiles)**0.5)
-  TILE_SIZE = len(list(tiles.values())[0])
-
-  def match_tiles(pos, idV, order):
-    matchH = pos % GRID_SIZE == 0 or get_border(
-        tilesV[idV], 'l') == get_border(tilesV[order[-1]], 'r')
-    matchV = pos < GRID_SIZE or get_border(
-        tilesV[idV], 'u') == get_border(tilesV[order[-GRID_SIZE]], 'd')
-    return matchH and matchV
-
-  def dfs(placed_tiles, seen):
-    if (pos := len(placed_tiles)) == len(tiles): return placed_tiles
+  tiles = {tile[0][5:-1]: tile[1:] for tile in puzzle}
+  anzTiles = len(tiles)
+  GRID_SIZE = int(anzTiles ** 0.5)
+  TILE_SIZE = len(puzzle[0][1])
+  tilesV = {idv: variante for id, tile in tiles.items() for idv, variante in rot_flip(id, tile)}
+  
+  def match_tiles(pos, idV, placed_idvs):
+    col, row = pos % GRID_SIZE, pos // GRID_SIZE
+    matchV = row == 0 or tilesV[idV][0] == tilesV[placed_idvs[pos-GRID_SIZE]][-1]  
+    matchH = col == 0 or [z[0] for z in tilesV[idV]] == \
+                         [z[-1] for z in tilesV[placed_idvs[pos-1]]]
+    return matchV and matchH    
+  
+  def dfs(pos,placed_idvs,seen_ids):
+    if pos == anzTiles: return placed_idvs
     for idV in tilesV:
-      if idV[0] in seen: continue
-      if match_tiles(pos, idV, placed_tiles):
-        if (solution := dfs(placed_tiles + [idV], seen | {idV[0]})): 
-          return solution
-
-  solution = dfs([], set())
-  part1 = math.prod(int(s[0]) for s in [solution[0],
-                                        solution[-1], solution[GRID_SIZE-1], solution[-GRID_SIZE]])
-
-  spacing = f'[.#\n]{{{GRID_SIZE*(TILE_SIZE-2)-19}}}'
-  monster = f'#.{spacing+"#....#"*3}##{spacing}.#{"..#"*5}'
+      if idV[0] in seen_ids: continue
+      if match_tiles(pos, idV, placed_idvs):
+        solution = dfs(pos+1, placed_idvs + [idV], seen_ids | {idV[0]})
+        if solution: return solution
+  
+  solution = dfs(0,[],set())
+  part1 = math.prod(int(idV[0]) for idV in [solution[0], solution[-1], solution[GRID_SIZE-1],solution[-GRID_SIZE]])
+  
   karte = create_map(solution, tilesV, GRID_SIZE, TILE_SIZE)
-
-  for _, karteV in get_vars('a', karte):
-    karteStr = '\n'.join(karteV)
-    if (monsters := len(re.findall(monster, karteStr, overlapped=True))):
-      return part1, sum(x == '#' for x in karteStr) - 15*monsters
-
+  pattern = r'#[.#]{77}#....##....##....###[.#]{77}#..#..#..#..#..#'
+  
+  for idv, variante in rot_flip('a', karte):
+    kartenStr = ''.join(variante)
+    monsters = len(re.findall(pattern, kartenStr, overlapped=True))
+    if monsters: return part1, kartenStr.count('#') - 15 * monsters
 
 puzzle = read_puzzle('Tag_20.txt')
 
