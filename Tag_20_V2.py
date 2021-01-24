e4b3f408881b67e6437793452edde0b252b4994c
Gravitar64 <gravitar@web.de>
2021-01-24 23:47:05 +0100

    youtube

--- Tag_20_V2.py
+++ Tag_20_V2.py
@@ -1,64 +1,62 @@
+from collections import defaultdict
 import time
 import math
-import regex as re
-from collections import defaultdict
+import regex
 
 
 def read_puzzle(file):
   with open(file) as f:
-    return [[x for x in tile.split('\n')] for tile in f.read().split('\n\n')]
+    return [[x for x in zeile.split('\n')] for zeile in f.read().split('\n\n')]
 
 
 def rot_flip(id, tile):
   for i in range(4):
     yield (id, i), tile
-    yield (id, i+4), tile[::-1]  # flipV
-    tile = list(map(''.join, zip(*tile[::-1])))  # rotate 90 degrees cw
+    yield (id, i+4), tile[::-1]  # vertikal gespiegelt
+    tile = list(map(''.join, zip(*tile[::-1]))) # rotiert um 90 Grad im Uhrzeigersinn
 
 
 def create_map(solution, tilesV, GRDS):
   karte = defaultdict(str)
-  for pos, idV in enumerate(solution):
-    tile_row = pos // GRDS
+  for pos, idv in enumerate(solution):
+    puzzle_zeile = pos // GRDS
     for zeile in range(8):
-      karte[tile_row*8+zeile] += tilesV[idV][zeile+1][1:9]
+      karte[puzzle_zeile*8+zeile] += tilesV[idv][zeile+1][1:9]
   return list(karte.values())
 
 
 def solve(puzzle):
   tiles = {int(tile[0][5:-1]): tile[1:] for tile in puzzle}
   anzTiles = len(tiles)
-  GRDS = int(anzTiles ** 0.5)
+  GRDS = int(math.sqrt(anzTiles))
   tilesV = {idv: variante for id, tile in tiles.items()
             for idv, variante in rot_flip(id, tile)}
 
-  def match_tiles(pos, idV, placed_idvs):
-    col, row = pos % GRDS, pos // GRDS
-    matchV = row == 0 or tilesV[idV][0] == tilesV[placed_idvs[pos-GRDS]][-1]
-    matchH = col == 0 or [z[0] for z in tilesV[idV]] == [
+  def match_tiles(pos, idv, placed_idvs):
+    spalte, zeile = pos % GRDS, pos // GRDS
+    matchH = spalte == 0 or [z[0] for z in tilesV[idv]] == [
         z[-1] for z in tilesV[placed_idvs[pos-1]]]
-    return matchV and matchH
+    matchV = zeile == 0 or tilesV[idv][0] == tilesV[placed_idvs[pos-GRDS]][-1]
+    return matchH and matchV
 
   def dfs(pos, placed_idvs, seen_ids):
-    if pos == anzTiles:
-      return placed_idvs
-    for idV in tilesV:
-      if idV[0] in seen_ids:
-        continue
-      if match_tiles(pos, idV, placed_idvs):
-        if (sol := dfs(pos+1, placed_idvs + [idV], seen_ids | {idV[0]})):
-          return sol
+    if pos == anzTiles: return placed_idvs
+    for idv in tilesV:
+      if idv[0] in seen_ids: continue
+      if match_tiles(pos, idv, placed_idvs):
+        solution = dfs(pos+1, placed_idvs + [idv], seen_ids | {idv[0]})
+        if solution: return solution
 
   sol = dfs(0, [], set())
-  part1 = math.prod(idV[0]
-                    for idV in [sol[0], sol[-1], sol[GRDS-1], sol[-GRDS]])
+  part1 = math.prod(idv[0] for idv in [sol[0], sol[-1], sol[GRDS-1], sol[-GRDS]])
 
-  karte = create_map2(sol, tilesV, GRDS)
+  karte = create_map(sol, tilesV, GRDS)
   pattern = r'#[.#]{77}#....##....##....###[.#]{77}#..#..#..#..#..#'
+  # puzzle = 12 Teile * 8 = 96 - 19 = 77
 
   for idv, variante in rot_flip('a', karte):
     kartenStr = ''.join(variante)
-    monsters = len(re.findall(pattern, kartenStr, overlapped=True))
+    monsters = len(regex.findall(pattern, kartenStr, overlapped=True))
     if monsters:
       return part1, kartenStr.count('#') - 15 * monsters
 
